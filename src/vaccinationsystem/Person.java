4fadadd834c212444c200cc42d5a2440f1d28020
Jesus <Jesus@Jesus-PC>
2017-11-24 15:23:01 +0200

    last edit for person class ( by  bebo 24/11 at 3:22 PM )

--- src/vaccinationsystem/Person.java
+++ src/vaccinationsystem/Person.java
@@ -5,42 +5,55 @@ package vaccinationsystem;
  */
 public abstract class Person {
     
-    protected int pId;
-    protected int pAge;
-    protected String pFirstName;
-    protected String pLastName;
-    protected String pEmail;
+    protected int pId ;
+    protected int pAge ;
+    protected String pFirstName ;
+    protected String pLastName ;
+    protected String pEmail ;
 
     
     public Person(int pId , int pAge , String pFirstName , String pLastName , String pEmail)
   {
-        this.pId = pId ;
-        this.pAge = pAge ; 
-        this.pFirstName = pFirstName ;
-        this.pLastName = pLastName ;
-        this.pEmail = pEmail ;
-  }
+      this.pId = pId ;
+      this.pAge = pAge ;
+      this.pFirstName = pFirstName ;
+      this.pLastName = pLastName ;
+      this.pEmail = pEmail ;
+      
+      }
     //OverRide ...
     @Override
+    
     public abstract String toString();
     
  //Setter
     public void setPId(int id){
         this.pId = id ;
     }    
-    public void setPAge(int age){
-        this.pAge = age ;
-    }
-        
+    
+    public boolean setPAge(int age){ // this function to check if age true or false
+        if(pAge < 0){
+          return false ;
+       } else {
+         this.pAge = pAge ;
+           return true ;
+      }
+    }  
     public void setPFirstName(String fname){
         this.pFirstName = fname ;
     }
         
     public void setPLastName(String lname){
         this.pLastName = lname ;
-    }    
-    public void setPEmail(String email){
-        this.pEmail = email ;
+    }  
+    
+    public boolean setPEmail(String email){ // this function to check if Ema@il true or false
+        if(!pEmail.contains("@") || !pEmail.contains(".") ){
+            return false ;
+       } else {
+         this.pEmail = pEmail ;
+            return true ;
+              }
     }    
     //Getter
     public int getPId(){
@@ -61,8 +74,6 @@ public abstract class Person {
     }    
     public String getPEmail(){
         return this.pEmail;
-    }
-    
- 
+    }  
     
 }
