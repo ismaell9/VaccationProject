7b91c7dd12e3f0d8f650dc563ffe01f0e4035620
Nagham <Nagham@Nagham>
2017-11-29 00:07:53 +0200

    7ad yshof ya gama3a :") XD

--- src/vaccinationsystem/Store.java
+++ src/vaccinationsystem/Store.java
@@ -1,47 +1,116 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vaccinationsystem;
 
-/**
- *
- * @author Reputation
- */
-public class Store {
+import java.util.*;
 
-/**
- * it's a good practice to stay "private"
- * however it's changeable
- */    
-    private int storeCapacity;
+public abstract class Store {
+
     private int storeId;
+    private int storeCapacity;
     private int storeFreeSpace;
 
-/**
- * setters && getters
- */
+    private final String StoreFileName = "Store.txt";
+    FileManger FManger = new FileManger();
+    public static ArrayList<Store> Store = new ArrayList<Store>();
+    
+    public Store(int storeCapacity, int storeId, int storeFreeSpace) {
+             
+        this.storeCapacity = storeCapacity;
+        this.storeId = storeId;
+        this.storeFreeSpace = storeFreeSpace;
+    }
+    //setter
+    public void setstoreCapacity(int storeCapacity) {
+        this.storeCapacity = storeCapacity;
+    }
 
+    public void setstoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public void setstoreFreeSpace(int storeFreeSpace) {
+        this.storeFreeSpace = storeFreeSpace;
+    }
+    //getter
+    public int getstoreId() {
+        return this.storeId;
+    }
+
+    public int getstoreFreeSpace() {
+        return this.storeFreeSpace;
+    }
+
+    public int getsetstoreCapacity() {
+        return this.storeCapacity;
+    }
+
+    
+    private int getvacIndex(int vacId){
+        for (int i = 0; i < Store.size(); i++)
+            if(Store.get(i).getvacId() == vacId)
+                return i;
+        return -1;
+    }
+    
+    
+    private String getstoreData() {
+        return this.storeId + "@" + this.storeFreeSpace + "@" + this.storeCapacity + "@" ;
+    }
+    
+    private void loadFromFile() {
+        
+        Store = (ArrayList<Store>) (Object) FManger.read(StoreFileName);
+    }
+    
+    private void commitToFile() {
+        
+        FManger.write(Store.get(0).getstoreData(), StoreFileName, false);
+        for (int i = 1; i < Store.size(); i++) {
+        FManger.write(Store.get(i).getstoreData(), StoreFileName, true);
+        }
+    }
+   /* 
+ */
+ 
 /**
  * search for vaccine by id
  * @param vacId
  * @return true if found, false if not
  */
     public boolean searchVac(int vacId){
-        return true;
+        loadFromFile();
+        int index = getvacIndex(vacId);
+        if(index != -1)
+            //return "\nFound ...!" + Store.get(index).toString();
+            return true;
+        else 
+            //return "\nNot Found ...!";
+             return false;
+
     }
+    
 /**
  * @return true if store is full, false if not
  */
     public boolean storeIsFull(){
-        return true;
-    }
-    
+       loadFromFile();
+       if(storeFreeSpace>=storeCapacity)
+       {
+           //System.out.println("Store Is FULL");
+            return true;
+            
+       }else{
+              return false;
+       }
+    }        
 /**
  * @return string of all information about the store
  */    
     public String storeReport(){
-        return "";
+        loadFromFile();
+        String S = "\nAll Store Data:\n";
+        for (Store x : Store) {
+            S = S + x.toString();
+        }
+        return S;
     }
 }
--- src/vaccinationsystem/StoreKeeper.java
+++ src/vaccinationsystem/StoreKeeper.java
@@ -1,37 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vaccinationsystem;
 
-import java.util.ArrayList;
-import static vaccinationsystem.Doctor.Doctors;
+import java.util.*;
 
-/**
- *
- * 
- 
- *
- */
 public class StoreKeeper extends staff{
 
     private Vaccine vac;
-
-private final String storekeeperFileName = "StoreKeeper.txt";
-  FileManger FManger = new FileManger();
-       public static ArrayList<StoreKeeper> StoreKeepers= new ArrayList<StoreKeeper>();
+    private final String storekeeperFileName = "StoreKeeper.txt";
+    FileManger FManger = new FileManger();
+    public static ArrayList<StoreKeeper> StoreKeepers= new ArrayList<StoreKeeper>();
   
   
     public StoreKeeper(int pId,int pAge,String pFirstName,String pLastName ,String pEmail,String sUsername,String sPassword,double sSalary){
-   super(pId,pAge,pFirstName,pLastName,pEmail,sUsername,sPassword,sSalary);
-}
-
-    StoreKeeper() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
+        super(pId,pAge,pFirstName,pLastName,pEmail,sUsername,sPassword,sSalary);
+    }   
     
     public boolean addstorekeeper() {
         if (this.FManger.write(this.getStoreKeeperData(), "StoreKeeper.txt" , true)) {
@@ -40,14 +21,14 @@ private final String storekeeperFileName = "StoreKeeper.txt";
             return false;
         }
 }
-         private String getStoreKeeperData() {
+    private String getStoreKeeperData() {
         return this.pId + "@" + this.pFirstName + "@" + this.pLastName + "@" + this.pAge + "@" + this.pEmail + "@" + this.sUserName + "@" + this.sPassword+ "@" + this.sSalary + "@";
     }
          
-         
-      private void loadFromFile() {
+    private void loadFromFile() {
            StoreKeepers = (ArrayList<StoreKeeper>) (Object) FManger.read("StoreKeeper.txt");
     }           
+    
     public String displayAllStoreKeeper() {
         loadFromFile();
         String S = "\nAll Professors Data:\n";
@@ -57,9 +38,7 @@ private final String storekeeperFileName = "StoreKeeper.txt";
         return S;
     }
          
-    
-    
-      private int getStoreKeeperIndex(int id){
+    private int getStoreKeeperIndex(int id){
         for (int i = 0; i < StoreKeepers.size(); i++)
             if(StoreKeepers.get(i).getPId()== id)
                 return i;
@@ -67,7 +46,7 @@ private final String storekeeperFileName = "StoreKeeper.txt";
         return -1;
     }
      
-public String searchStoreKeeper(int id){
+    public String searchStoreKeeper(int id){
         loadFromFile();
         int index = getStoreKeeperIndex(id);
         if(index != -1)
@@ -76,7 +55,7 @@ public String searchStoreKeeper(int id){
             return "\nNot Found ...!";
     }    
     
- private void commitToFile() {
+    private void commitToFile() {
         FManger.write(StoreKeepers.get(0).getStoreKeeperData(), storekeeperFileName, false);
         for (int i = 1; i <StoreKeepers.size(); i++) {
             FManger.write(StoreKeepers.get(i).getStoreKeeperData(), storekeeperFileName, true);
@@ -84,9 +63,7 @@ public String searchStoreKeeper(int id){
 
     }
  
- 
-
-public void updateStore(int oldID, StoreKeeper x){
+    public void updateStore(int oldID, StoreKeeper x){
         loadFromFile();
         int index = getStoreKeeperIndex(oldID);
         StoreKeepers.set(index, x);
@@ -100,67 +77,52 @@ public void updateStore(int oldID, StoreKeeper x){
         commitToFile();
     } 
     
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
-         
     @Override
     public String toString() {
         return "I'm Store Keeper : " + pFirstName + " " + pLastName + "\n" + "ID : " + pId + " Age : " + pAge + "\n"
                 + "\nUserName: " + sUserName + "\t Password: " + sPassword + "\nSalary: " + sSalary;
     }
-
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //use "vac" object to use methods in "Vaccine" class
     public void set_vac(Vaccine vac){
         this.vac = vac;
     }
-//use "vac" object to use methods in "Vaccine" class
-
-    
     
     
 /**
  * Create instance of Store class for aggregation
  * It's completely changeable
  */
+    
+    //use "store" object to use methods in "Store" class
     private Store store;
     
     public void set_store(Store store){
         this.store = store;
     }
-//use "store" object to use methods in "Store" class
  
-    
+   private int getvacIndex(int vacId){
+        for (int i = 0; i < vac.size(); i++)
+            if(vac.get(i).getvacId() == vacId)
+                return i;
+        return -1;
+    }
+
 /**
  * access modifier is changeable 
  * remember it's better to stay "private"
  * @return true if found, false if not
  */
-    public boolean searchVac(){
-        return true;
+     public boolean searchVac(int vacId){
+        loadFromFile();
+        int index = getvacIndex(vacId);
+        if(index != -1)
+            //return "\nFound ...!" + Store.get(index).toString();
+            return true;
+        else 
+            //return "\nNot Found ...!";
+             return false;
+
     }
 
 }
