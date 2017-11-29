30521c9f0a10a6c0157d606fe0aafc8377554d09
Reputation <Reputation@A>
2017-11-29 16:39:35 +0200

    Update client class

--- src/vaccinationsystem/Client.java
+++ src/vaccinationsystem/Client.java
@@ -11,15 +11,40 @@ public class Client extends Person{
     private int cPhoneNumber;
     private String cAdress;
     private String cGender;
-    
-/**
- * Constructor
- * @param 
- */
-    public Client(){
-        
+
+    public Client(int pId, int pAge, String pFirstName, String pLastName, String pEmail,int cPhoneNumber, String cAdress, String cGender) {
+        super(pId, pAge, pFirstName, pLastName, pEmail);
+        this.cPhoneNumber = cPhoneNumber;
+        this.cAdress = cAdress;
+        this.cGender = cGender;
     }
-    
+/**
+ * Setters && Getters
+ * @param cPhoneNumber 
+ */
+    public void set_cPhoneNumber(int cPhoneNumber){
+        this.cPhoneNumber = cPhoneNumber;
+    }
+    public void set_cAdress(String cAdress){
+        this.cAdress = cAdress;
+    }
+
+    public void set_cGender(String cGender){
+        this.cGender = cGender;
+    }
+
+    public int get_cPhoneNumber(){
+        return this.cPhoneNumber;
+    }
+    public String get_cAdress(){
+        return this.cAdress;
+    }
+    public String get_cGender(){
+        return this.cGender;
+    }
+/**
+ * -----------------------------------------------------------------------------------------------------------------------------------------------------
+ */
 /**
  * method return type may be changed to "void"
  * @return true if found, false if not
@@ -66,6 +91,11 @@ public class Client extends Person{
  * @return array of client objects from clients file
  */
     public Object[] listClient(){
+        return null;
+    }
+
+    @Override
+    public String toString() {
         return null;
     }
 
